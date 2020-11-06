cad0fc2d35d743615646f1783e48e952679a0bae
Daniel Dziura <danieldziura@paucasesnovescifp.cat>
2020-11-06 19:21:28 +0100

    Unit 1 Windows Forms Components Common Events
    Componentes:
    Form
    Name: mainForm
    Text: Windows Forms Components - Common Events
    TextBox
    Name: messagesTextBox
    Multiline: True
    ReadOnly: True
    BackColor: New Custom Color R = 25 G= 25, B = 25
    ForeColor: White
    ScrollBars: Vertical
    Label
    NumericUpDown
    ListBox
    TextBox
    PictureBox
    
    Se usan los eventos: LostFocus, GotFocus, KeyPress, ScrollBars, Multiline, ValueChanged, SelectedIndexChanged, MouseEnter y MouseLeave.

---
+++ WinformComponents/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace WinformComponents
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.messagesTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // messagesTextBox
+            // 
+            this.messagesTextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.messagesTextBox.ForeColor = System.Drawing.Color.White;
+            this.messagesTextBox.Location = new System.Drawing.Point(374, 12);
+            this.messagesTextBox.Multiline = true;
+            this.messagesTextBox.Name = "messagesTextBox";
+            this.messagesTextBox.ReadOnly = true;
+            this.messagesTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.messagesTextBox.Size = new System.Drawing.Size(414, 426);
+            this.messagesTextBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(61, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(64, 71);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown1.TabIndex = 2;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Items.AddRange(new object[] {
+            "Primero",
+            "Segundo",
+            "Tercero"});
+            this.listBox1.Location = new System.Drawing.Point(64, 135);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 84);
+            this.listBox1.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(64, 251);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 4;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(64, 307);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(136, 116);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.messagesTextBox);
+            this.Name = "Form1";
+            this.Text = "Windows Forms Components - Common Events";
+            this.Load += new System.EventHandler(this.mainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox messagesTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ WinformComponents/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinformComponents
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            textBox1.Multiline = true;
+            textBox1.ScrollBars = ScrollBars.Both;
+
+            textBox1.KeyPress += TextBox1_KeyPress;
+
+            textBox1.LostFocus += TextBox1_LostFocus;
+            textBox1.GotFocus += TextBox1_GotFocus;
+
+            numericUpDown1.LostFocus += NumericUpDown1_LostFocus;
+            numericUpDown1.GotFocus += NumericUpDown1_GotFocus;
+
+            label1.LostFocus += Label1_LostFocus;
+            label1.GotFocus += Label1_GotFocus;
+
+            numericUpDown1.ValueChanged += NumericUpDown1_ValueChanged;
+
+            listBox1.SelectedIndexChanged += ListBox1_Index;
+
+            pictureBox1.MouseEnter += PictureBox1_MouseEnter;
+            pictureBox1.MouseLeave += PictureBox1_MouseLeave;
+        }
+        private void ListBox1_Index(object sender, System.EventArgs e)
+        {
+            String a = listBox1.SelectedIndex.ToString();
+            logger("ListBox1 Index: " + a);
+        }
+
+        private void PictureBox1_MouseEnter(object sender, System.EventArgs e)
+        {
+            logger("PictureBox1_MouseEnter");
+        }
+        private void PictureBox1_MouseLeave(object sender, System.EventArgs e)
+        {
+            logger("PictureBox1_MouseLeave");
+        }
+
+        private void mainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected override void OnClick(EventArgs e)
+        {
+            logger("OnClick");
+        }
+
+        protected override void OnDoubleClick(EventArgs e)
+        {
+            logger("OnDoubleClick");
+        }
+
+        
+        private void TextBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            messagesTextBox.AppendText($"KeyPress keychar: {e.KeyChar}" + "\r\n");
+        }
+
+        //Focus -----------
+        private void TextBox1_LostFocus(object sender, System.EventArgs e)
+        {
+            logger("TextBox1_LostFocus");
+        }
+        private void TextBox1_GotFocus(object sender, System.EventArgs e)
+        {
+            logger("TextBox1_GotFocus");
+        }
+        private void NumericUpDown1_LostFocus(object sender, System.EventArgs e)
+        {
+            logger("NumericUpDown1_LostFocus");
+        }
+
+        private void NumericUpDown1_GotFocus(object sender, System.EventArgs e)
+        {
+            logger("NumericUpDown1_GotFocus");
+        }
+
+        private void Label1_GotFocus(object sender, System.EventArgs e)
+        {
+            logger("Label1_GotFocus");
+        }
+        private void Label1_LostFocus(object sender, System.EventArgs e)
+        {
+            logger("NumericUpDown1_LostFocus");
+        }
+
+
+        //End Focus ---------------
+
+        private void NumericUpDown1_ValueChanged(Object sender, EventArgs e)
+        {
+            String a = numericUpDown1.Value.ToString();
+            logger("NumericUpDown1_ValueChanged to " + a);
+        }
+
+        public void logger(String evento)
+        {
+            //messagesTextBox.Text = (evento + Environment.NewLine);
+            messagesTextBox.Text += System.Environment.NewLine + evento;
+            //MessageBox.Show("You are in the Control.GotFocus event.");
+        }
+    }
+}
